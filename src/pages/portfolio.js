e565ab2e70930bc0f5e0d0575afd2e7f21d60546
Hernie Jabien <herniejabien45@gmail.com>
2021-02-03 14:50:39 +0800

    Added json projects

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,69 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Portfolio() {
-    const myprojects = [
-        {
-            key: 0,
-            project: "Sample",
-            screenshot: "img/freeimg_89251639freejpg850.jpg",
-            date: "2018 - 2020",
-            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint, nisi earum. Neque natus.",
-            tag: "web",
-        },
-        {
-            key: 1,
-            project: "Sample Project 2",
-            screenshot: "img/freeimg_89251639freejpg850.jpg",
-            date: "2019",
-            description: "Amet consectetur adipisicing elit. Sint, lorem ipsum dolor sit, nisi earum. Neque natus.",
-            tag: "mobile",
-        },
-        {
-            key: 2,
-            project: "Sample Project 3",
-            screenshot: "img/freeimg_89251639freejpg850.jpg",
-            date: "2019",
-            description: "Amet consectetur adipisicing elit. Sint, lorem ipsum dolor sit, nisi earum. Neque natus.",
-            tag: "mobile",
-        }
-    ];
     const [projects, setProjects] = useState([]);
     const [category, setCatagory] = useState("all");
-
-
-    function addProject(project) {
-        console.log(projects);
-        // console.log(project.tag === projects[0].tag);
-        if (projects.length !== 0 && projects[0].tag === project.tag) {
-            setProjects([
-                ...projects,
-                project
-            ]);
-        } else {
-            setProjects([
-                project
-            ]);
-        }
-    }
-
-    const getProjects = (e) => {
-        if (category !== e.target.id) {
-
-            if (e.target.id === "all") {
-                setProjects(myprojects);
-            } else {
-                myprojects.forEach(project => {
-                    if (project.tag === e.target.id) {
-                        addProject(project);
-                    }
-                });
-
+    
+    const getProjects = ()=>{
+        fetch(
+            'json/projects.json',
+            {
+                headers: {
+                    'Content-Type': "application/json",
+                    'Accept': "application/json"
+                }
             }
-
-            setCatagory(e.target.id);
-        }
-
+        ).then(
+            function(response){
+                return response.json();
+            }
+        ).then(function(jsonData){
+            console.log(jsonData);
+            setProjects(jsonData);
+        });
     };
+    
+    const getByCategory = (e) => {
+        setCatagory(e.target.id.replaceAll("menu_", ""));
+    };
+
+    useEffect(()=>{
+        getProjects();
+    },[]);
 
     return (
         <div id="portfolio" class="">
@@ -71,52 +37,41 @@ function Portfolio() {
                 <h4 class="font-weight-bold text-primary-default display-inline">
                     Portfolio
                 </h4>
-                <div class="nav-tab pt-2 pb-3 px-3 mt-2 crv-8 bg-light">
-                    <ul class="list-unstyled">
-                        <li class="float-left mr-3" id="all" onClick={getProjects}>All</li>
-                        <li class="float-left mr-3" id="web" onClick={getProjects}>Websites</li>
-                        <li class="float-left mr-3" id="mobile" onClick={getProjects}>Mobile App</li>
-                        <li class="float-left mr-3" id="others" onClick={getProjects}>Others</li>
+                <div class="nav-tab pt-2 px-3 pb-4 mt-2 crv-8 bg-light">
+                    <ul class="list-unstyled mb-4">
+                        <li class="float-left mr-3 btn py-2" id="menu_all" onClick={getByCategory}>All</li>
+                        <li class="float-left mr-3 btn py-2" id="menu_web" onClick={getByCategory}>Websites</li>
+                        <li class="float-left mr-3 btn py-2" id="menu_mobile" onClick={getByCategory}>Mobile Apps</li>
+                        <li class="float-left mr-3 btn py-2" id="menu_others" onClick={getByCategory}>Others</li>
                     </ul>
                 </div>
 
                 <div class="p-3">
                     <div class="row">
                         {
-                            projects.length !== 0 ?
-                                projects.map(project => (
-                                    (category === project.tag || category === "all")?
-                                    <div class="col-md-4 mt-3" key={project.key}>
-                                        <div class="card p-0">
+                            projects.map(project =>
+                            (
+                                category === "all" || category === project.tag ?
+                                    <div class="col-md-4 mt-3 animated bounceIn" key={project.key}>
+                                        <div class="card p-0 h-100">
                                             <div class="card-header p-0">
                                                 <img src={project.screenshot} alt="" class="img-fluid" />
                                             </div>
                                             <div class="card-body p-2">
                                                 <p class="font-weight-bold mb-0">
                                                     {project.project} <small>( {project.date} )</small>
+                                                    {
+                                                        project.status === "unavailable" ?
+                                                            <span className="ion-android-lock ml-2"></span>
+                                                            : <span className="ion-social-github ml-2"></span>
+                                                    }
                                                 </p>
                                                 <span>{project.description}</span>
                                             </div>
                                         </div>
                                     </div>
-                                    : <div className="" key={project.key}></div>
-
-                                )) :
-                                myprojects.map(project => (
-                                    <div class="col-md-4 mt-3" key={project.key}>
-                                        <div class="card p-0">
-                                            <div class="card-header p-0">
-                                                <img src={project.screenshot} alt="" class="img-fluid" />
-                                            </div>
-                                            <div class="card-body p-2">
-                                                <p class="font-weight-bold mb-0">
-                                                    {project.project} <small>( {project.date} )</small>
-                                                </p>
-                                                <span>{project.description}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))
+                                    : <div key={project.key}></div>
+                            ))
                         }
                     </div>
                 </div>
